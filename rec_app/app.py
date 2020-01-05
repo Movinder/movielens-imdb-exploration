d8cfd6e0fd2d831482587172c80d3b325f832f4c
Can Altıniğne <canaltinigne@gmail.com>
2020-01-05 02:34:42 +0100

    number of people fixed in the app

--- rec_app/app.py
+++ rec_app/app.py
@@ -1,10 +1,16 @@
-import flask
+from flask import Flask, request, render_template
 
-app = flask.Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return(flask.render_template('main.html'))
+
+    if request.method == 'POST':
+        count = int(request.form.get('people-select'))
+        return(render_template('main.html', settings = {'showVote': True, 'people': count, 'buttonDisable': True}))
+    elif request.method == 'GET':
+       return(render_template('main.html', settings = {'showVote': False, 'people': 0, 'buttonDisable': False}))
+    
 
 if __name__ == '__main__':
     app.run()
